30d497829ee09681ae4c1f521bd8ed3d95a1dde0
Jesusmendoza11 <160240333+Jesusmendoza11@users.noreply.github.com>
2024-08-10 07:45:54 -0500

    Add files via upload

---
+++ animal/animal.py
@@ -0,0 +1,7 @@
+class Animal:
+    def __init__(self,Nombre, Peso):
+        self.Nombre = Nombre
+        self.Peso = Peso
+        
+    def __str__(self):
+        return f"El nombre es: {self.Nombre}, Peso: {self.Peso}"
